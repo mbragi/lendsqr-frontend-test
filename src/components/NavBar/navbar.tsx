5e47b257f21ab14a0ee913721b3db7e9c6d066e5
m@nji <spontaneousmanji@gmail.com>
2023-01-03 23:53:07 +0100

    styling navbar search field

--- src/components/NavBar/navbar.tsx
+++ src/components/NavBar/navbar.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import union from '../../assests/Union.png'
 import lend from '../../assests/lendsqr.png'
+import notify from '../../assests/notify.png'
+import dropdown from '../../assests/dropdown.png'
+import search from '../../assests/search.png'
+
 import "./navbar.scss"
 
 function Navbar() {
@@ -11,15 +15,21 @@ function Navbar() {
           <img src={lend} alt="lendsqr" width='100' height='35' />
         </div>
         <div className='search-input-container'>
-         <input type="text" placeholder='search for everything' />
-         <button>
-          <span>
-           <img src="" alt="search" />
+         <input type="text" placeholder='search for everything' className='search-input' />
+         <button className='search-button'>
+          <span >
+           <img src={search} alt="search" width='10'/>
           </span>
          </button>
         </div>
         <div className='notifications-container'>
-
+       <a href="/">Docs</a>
+       <img  src={notify} alt="notify" />
+       <div className='dropdown-section'>
+       <img src="" alt="userProfile" />
+       <p>firstName</p>
+       <img src={dropdown} alt="dropdown" />
+       </div>
         </div>
     </div>
   )
