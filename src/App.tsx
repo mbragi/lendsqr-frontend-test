be9c6142ee67573044be33cfbc49260e6854f002
m@nji <spontaneousmanji@gmail.com>
2023-01-03 01:46:07 +0100

    feat: Implemented Login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Login from "./pages/login/login";
 
 function App() {
   return (
-    <div style={{width:'100%',height:'100%',display:'flex',flexDirection:'column'}}>
+    <div >
       <Login/>
     </div>
   );
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,41 @@
+import "./login.scss";
 import React from 'react'
+import union from '../../assests/Union.png'
+import lend from '../../assests/lendsqr.png'
+import pablo from '../../assests/pablo-sign-in 1.png'
 
 function Login() {
+ const httpSignIn = async () => {
+    console.log('redirect to login')
+ }
   return (
-    <div>login</div>
+    <React.Fragment>
+      <section className='login-container'>
+      <div className='left'>
+        <div className="logo-container">
+          <img src={union} alt="union" width='30' height='25'/>
+          <img src={lend} alt="lendsqr" width='100' height='35' />
+        </div>
+        <div className="login-icon">
+          <img src={pablo} alt="pablo" />
+        </div>
+      </div>
+      <div className='right'>
+        <form onSubmit={httpSignIn} className='login-form-container'>
+          <div className="login-form-title">
+            <h2>Welcome!</h2>
+            <p>Enter details to login.</p>
+          </div>
+          <div className="login-form-input">
+          <input className="email" type="email" name="email" placeholder="Email" autoComplete='off'  onChange={(e)=>{console.log(e.target.value)}}/>
+          <input className="password" type="password" name="password" placeholder="Password" autoComplete="off" onChange={(e)=>{console.log(e.target.value)}}/>
+          </div>
+          <a href='/' className="forgot-password">FORGOT PASSWORD? </a> 
+          <button className="submit" type="submit" >Log In</button>
+        </form>
+      </div>
+      </section>
+    </React.Fragment>
   )
 }
 
